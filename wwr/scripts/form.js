568bb50f33f07b6575a7a11f931f4fdc5d794f4d
conwads <Wadsworthjc97@gmail.com>
2022-03-28 14:22:48 -0600

    contact page changes

--- wwr/scripts/form.js
+++ wwr/scripts/form.js
@@ -1,3 +1,4 @@
+
     // get the feedback div element so we can do something with it.
     const feedbackElement = document.getElementById('feedback');
     // get the form so we can read what was entered in it.
@@ -7,11 +8,9 @@
         // stop the form from doing the default action.
         e.preventDefault();
         // set the contents of our feedback element to a message letting the user know the form was submitted successfully. Notice that we pull the name that was entered in the form to personalize the message!
-        feedbackElement.innerHTML = 'Hello '+ formElement.user_name.value +'! Thank you for your message. We will get back with you as soon as possible!';
+        feedbackElement.innerHTML = 'Hello '+ formElement.name.value +'! Thank you for your message. We will get back with you as soon as possible!';
         // make the feedback element visible.
         feedbackElement.style.display = "block";
         // add a class to move everything down so our message doesn't cover it.
         document.body.classList.toggle('moveDown');
     });
-
-    
